b1e15912d0c6d4bf6720c73cdf0fb20946fc066f
Sarah-Obarhoro <105218020+Sarah-Obarhoro@users.noreply.github.com>
2023-06-28 05:21:51 +0100

    Installed react-router-dom

--- techlift-mart/src/App.jsx
+++ techlift-mart/src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "./Pages/LandingPage";
 import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
